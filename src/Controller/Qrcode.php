a5253c511f8088d80f486223152bfc92ef2feddb
twinh <twinhuang@qq.com>
2016-12-01 15:51:56 +0800

    移除注释代码,屏蔽外部方法,解决qrcode自动测试问题 #2053

--- src/Controller/Qrcode.php
+++ src/Controller/Qrcode.php
@@ -32,50 +32,11 @@ class Qrcode extends \miaoxing\plugin\BaseController
         // 精度越高,图片越大,识别率越高,取值为0 - 4
         $level = $req['level'] ?: 3;
 
-        $logo = $req['logo'];
-
-        $logoSize = $req['logoSize'] ?: 30;
-
         // 生成二维码图片资源对象
         $enc = \QRencode::factory($level, $size, 0);
         $tab = $enc->encode($text);
         $maxSize = (int) (QR_PNG_MAXIMUM_SIZE / (count($tab)));
         $image = $this->image($tab, min($size, $maxSize), 0);
-
-        // TODO 支持远程图片
-        // 将LOGO放置到图片中间
-        /*if ($logo && substr($logo, 0, 4) != 'http') {
-            if (!file_exists($logo)) {
-                return $this->err('图片无效或者不存在');
-            }
-
-            $logoImg = null;
-            switch (strtolower(substr($logo, strrpos($logo, '.') + 1))) {
-                case 'jpg':
-                    $logoImg = imagecreatefromjpeg($logo);
-                    break;
-                case 'png':
-                    $logoImg = imagecreatefrompng($logo);
-                    break;
-                case 'gif':
-                    $logoImg = imagecreatefromgif($logo);
-                    break;
-                default:
-                    return $this->err('不支持该图片类型');
-            }
-
-            $bgX = imagesx($image);
-            $bgY = imagesy($image);
-            $logoX = imagesx($logoImg);
-            $logoY = imagesy($logoImg);
-
-            // 缩放logo
-            $tmpImg = imagecreatetruecolor($logoSize, $logoSize);
-            imagecopyresampled($tmpImg, $logoImg, 0, 0, 0, 0, $logoSize, $logoSize, $logoX, $logoY);
-            imagecopyresized($image, $tmpImg, ($bgX - $logoSize) / 2, ($bgY - $logoSize) / 2, 0, 0, $logoSize, $logoSize, $logoSize, $logoSize);
-            imagedestroy($logoImg);
-            imagedestroy($tmpImg);
-        }*/
 
         // 生成图片内容
         ob_start();
@@ -112,6 +73,9 @@ class Qrcode extends \miaoxing\plugin\BaseController
      * @param int $back_color
      * @param int $fore_color
      * @return resource
+     *
+     * @codingStandardsIgnoreFile
+     * @SuppressWarnings(PHPMD)
      */
     protected function image(
         $frame,
